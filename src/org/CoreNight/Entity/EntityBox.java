b2ebed203e34da6383dec184d9c24570c7567fe6
mtds27 <scolarimathew@gmail.com>
2016-12-04 13:58:15 -0800

    Added color pickup checking

--- src/org/CoreNight/Entity/EntityBox.java
+++ src/org/CoreNight/Entity/EntityBox.java
@@ -4,6 +4,8 @@ import org.CoreNight.GameManager;
 import org.CoreNight.Main;
 
 import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -43,8 +45,35 @@ public class EntityBox extends Entity{
         if(this.y + this.height > GameManager.player.y && this.row == GameManager.player.row && !(this instanceof EntityPlayer)){
             GameManager.addColor(this.color);
             EntityManager.remove(this);
-            if(this.color.equals(Color.GRAY) || this.color.equals(Color.LIGHT_GRAY) || this.color.equals(Color.DARK_GRAY)){
-                System.exit(0);
+            if(this.color.equals(Color.WHITE)){
+                if (GameManager.player.lives == 1) {
+                    System.exit(0);
+                }
+                else {
+                    GameManager.player.lives -= 1;
+                }
+            }
+
+            if (this.color.equals(Color.BLUE)) {
+                System.out.println("Blue color hit");
+                float origSpeed = GameManager.speed;
+                GameManager.speed = 1;
+                Timer t = new Timer( );
+                t.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        GameManager.speed = origSpeed;
+                        GameManager.background = new Color(0, 0, 0);
+                    }
+                }, 5000,1000);
+            }
+
+            if (this.color.equals(Color.GREEN)) {
+                GameManager.player.lives += 1;
+            }
+
+            if (this.color.equals(Color.RED)) {
+                // put red ability code here
             }
         }
     }
